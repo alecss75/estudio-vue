19ee38ccb98344766b5f85ba74146669c6ca6b0b
alecss75 <alejandrosansoresarjona@gmail.com>
2024-05-19 14:52:19 -0500

    COMPONENTES: Creación de componentes -en Option API -en Composition API Apuntes

---
+++ helloworld/src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="post-card">
+    <h3>Título</h3>
+   <p>Lorem ipsum dolor sit amet consectatur blablabbla</p>
+   <PostButton/>
+</div>  
+</template>
+
+<script lang="ts" setup>
+    import PostButton from './PostButton.vue'
+</script>
+
+<style scoped>
+.post-card {
+    border-radius:15px;
+    padding:10px;
+    margin:5px;
+    border: 1px solid #ccc;
+}
+</style>
